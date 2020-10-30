891b9491ebeb5bdec5f6e56d91ec7969c7b5f587
therajvikash <vchy63660@gmail.com>
2020-10-30 13:29:54 +0530

    OpenDesk First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import EmployeePortal from "./components/EmployeePortal";
+
+
+function App() {
+  return (
+    <div className="App">
+        <EmployeePortal/>
+    </div>
+  );
+}
+
+export default App;
